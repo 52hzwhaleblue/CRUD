96f3c823a9be9a8a8195d5d2dbd2ef90afdc2abe
DELL <DELL@DESKTOP-CG3KOCH>
2022-06-09 23:24:25 +0700

    thay doi edit

---
+++ .history/app/Http/Controllers/ProductCatController_20220609221210.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\ProductCat;
+
+class ProductCatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+           // $data = DB::table('product_cats')
+           // ->whereJsonContains('status', 'noibat')
+           // ->get();
+
+           // var_dump($data);
+           // die();
+
+            $data = ProductCat::get();
+            return view('admin.product-cat.index',[
+                'data' => $data,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.product-cat.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $countProductCat = ProductCat::all()->count();
+
+        if($request->has('image')){
+        $file= $request->image;
+        $ext = $request->image->extension();//lấy đuôi file png||jpg
+        $file_name = Date('Ymd').'-'.'product'.$countProductCat.'.'.$ext;
+        $file->move(public_path('backend/assets/img/products'),$file_name);//chuyển file vào đường dẫn chỉ định
+        }
+
+
+        $product_cat = new ProductCat;
+        $product_cat->name = $request->get('name');
+        $product_cat->desc = $request->get('desc');
+        $product_cat->content = $request->get('content');
+        $product_cat->photo = $file_name;
+        $product_cat->status = implode(',', $request->get('status'));
+
+        $product_cat->save();
+
+        return redirect()->route('product_cat.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductCat $ProductCat)
+    {
+        return view('admin.product-cat.edit',[
+        'each' => $ProductCat,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,ProductCat $ProductCat)
+    {
+        // $ProductCat sẽ validate xem có tồn tại id hay chưa, không cần xét điều kiện where
+        $fix_status = implode(',', $request->get('status'));
+            $ProductCat->update(
+            [
+                'name' => $request->get('name'),
+                'desc' => $request->get('desc'),
+                'content' => $request->get('content'),
+                'status'=> $fix_status,
+            ],
+            $request->except([
+                '_token',
+                '_method',
+            ])
+        );
+
+    return redirect()->route("product_cat.index");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCat $ProductCat)
+    {
+        $ProductCat->delete();
+        return redirect()->route('product_cat.index');
+    }
+}
---
+++ .history/resources/views/Admin/news/create.blade_20220609205754.php
@@ -0,0 +1,47 @@
+@extends('admin.layout')
+@section('title')
+    Chỉnh sửa sản phẩm cấp 1
+@endsection
+@section('content')
+<form method="post" action="{{ route('news.store') }}"  enctype="multipart/form-data" >
+    @csrf
+    <div class="card-body">
+        <div class="form-group">
+            <label for="menu">Ảnh Sản Phẩm</label>
+            <input type="file" name="image" class="form-control" id="upload">
+            <input type="hidden"  id="image">
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="menu">Tên Sản Phẩm</label>
+                    <input type="text" name="name" class="form-control"  placeholder="Nhập tên sản phẩm">
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label>Mô Tả </label>
+            <input name="desc" class="form-control"></input>
+        </div>
+
+        <div class="form-group">
+            <label>Mô Tả Chi Tiết</label>
+            <textarea name="content" id="cke_content" class="form-control"></textarea>
+        </div>
+
+          <div class="form-group">
+            <label>Trạng thái</label>
+            <div class="custom-control custom-radio">
+                <input type="checkbox" name="status[]" value="noibat"> Nổi bật
+                <input type="checkbox" name="status[]" value="hienthi"> Hiển thị             
+            </div>
+        </div> 
+    </div>
+
+    <div class="card-footer">
+        <button type="submit" class="btn btn-primary">Thêm Sản Phẩm</button>
+    </div>
+    @csrf
+</form>
+@endsection
