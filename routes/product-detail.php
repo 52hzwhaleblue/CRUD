83ad5772586a82ff7fdae7fd52c46056a41b5e32
Mr. Long <josephminhlong@gmail.com>
2022-06-20 22:24:46 +0700

    Long Xóa Phân Quyền Route

--- routes/product-detail.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductDetailController;
-
-
-Route::get('/product-detail/{id}',[ProductDetailController::class,'index'])->name("user.product_detail");
